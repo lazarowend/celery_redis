d6c25bcd6f6e9af9e4bc152462f6cb0045206f0a
lazarowend <lazaroclans1@gmail.com>
2024-05-09 18:27:08 -0300

    add countdown

--- tasks.py
+++ tasks.py
@@ -4,11 +4,11 @@ from random import randint
 app = Celery(broker='redis://127.0.0.1:6379/0')
 
 
-@app.task(bind=True, default_retry_delay=2, max_retries=3, autoretry_for=(ZeroDivisionError, ))
+@app.task(bind=True, default_retry_delay=2, max_retries=10, autoretry_for=(ZeroDivisionError, ))
 def exibir(self):
     n1 = randint(1, 10)
     if n1 < 5:
-        self.retry()
+        self.retry(countdown = 2*self.request.retries)
         raise ZeroDivisionError('erro kkkkk')
     
     return 'ok'
